e302d652fc90f82c7d931f423f2dd532c532d714
gamesketches <sam.von.ehren@gmail.com>
2015-09-25 11:54:19 -0400

    Towers can now contain checkers, so that's exciting

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ function create() {
   for(var i = 0; i < 600; i += 130)
   {
     var tower = towerGroup.create(i + 50, 350, 'wallTexture');
+    tower.checkers = Math.round(Math.random() * 5);
+  }
+  for(var i = 0; i < towerGroup.children.length; i++){
+    var tower = towerGroup.children[i];
+    var text = game.add.text(tower.x + 50, 330, "",
+                        { font: "20px Arial", fill: "#ffFFFF", align: "center" });
+    text.anchor.setTo(0.5, 0.5);
+    text.setText(tower.checkers);
   }
 }
 
