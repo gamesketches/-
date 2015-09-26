e62f8888bc7c84129808dab2fb4deab188620f74
gamesketches <sam.von.ehren@gmail.com>
2015-09-26 01:33:36 -0400

    Got the towers to change hands, still some funny business but w/e

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ var diceRoll;
 var selectedTower = null;
 
 function selectTower(sprite, pointer) {
-      if(selectedTower != null){
+      if(selectedTower != null && (sprite.checkers <= 1 || sprite.text.fill == selectedTower.text.fill)){
         sprite.checkers += 1;
         selectedTower.checkers -= 1;
+        sprite.text.fill = selectedTower.text.fill;
         sprite.text.setText(sprite.checkers);
         selectedTower.text.setText(selectedTower.checkers);
         selectedTower = null;
       }
-      else {
+      else if(sprite.checkers > 0){
         selectedTower = sprite;
       }
 }
@@ -37,8 +38,14 @@ function create() {
   }
   for(var i = 0; i < towerGroup.children.length; i++){
     var tower = towerGroup.children[i];
+    if(i < towerGroup.children.length / 2){
     tower.text = game.add.text(tower.x + 50, 330, "",
-                        { font: "20px Arial", fill: "#ffFFFF", align: "center" });
+                        { font: "20px Arial", fill: "#ff0000", align: "center" });
+    }
+    else {
+      tower.text = game.add.text(tower.x + 50, 330, "",
+                          { font: "20px Arial", fill: "#ffffFF", align: "center" });
+    }
     tower.text.anchor.setTo(0.5, 0.5);
     tower.text.setText(tower.checkers);
   }
