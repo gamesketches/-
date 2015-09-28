f669611c2630ce5d30288d2e6feae3e0942ccce7
gamesketches <sam.von.ehren@gmail.com>
2015-09-28 11:42:09 -0400

    Tracking actions, forbidding players from touch other player's pieces

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ var diceRoll;
 
 var selectedTower = null;
 
+var currentPlayer = "#ffffff";
+var actionsTaken = 0;
+
+var player1 = "#ffffff";
+
+var player2 = "#FF0000";
+
 function selectTower(sprite, pointer) {
+      // Move checkers from one tower to another
       if(selectedTower != null && (sprite.checkers <= 1 || sprite.text.fill == selectedTower.text.fill)){
         sprite.checkers += 1;
         selectedTower.checkers -= 1;
@@ -16,9 +24,31 @@ function selectTower(sprite, pointer) {
         sprite.text.setText(sprite.checkers);
         selectedTower.text.setText(selectedTower.checkers);
         selectedTower = null;
+        // Record an action taken, switch players if currentPlayer has taken 2 actions
+        actionsTaken++;
+        if(actionsTaken >= 2) {
+          currentPlayer = (currentPlayer == player2) ? player1 : player2;
+          console.log(currentPlayer);
+          actionsTaken = 0;
+        }
       }
+      // Select a tower if it has checkers
       else if(sprite.checkers > 0){
+        // If attempting to bear off your troops
+        if((towerGroup.children[0] == sprite && currentPlayer == player1) ||
+              towerGroup.children[towerGroup.children.length - 1] == sprite && currentPlayer == player2)
+        {
+            sprite.checkers -=1;
+            sprite.text.setText(sprite.checkers);
+            actionsTaken++;
+            if(actionsTaken >= 2) {
+              currentPlayer = (currentPlayer == player2) ? player1 : player2;
+              actionsTaken = 0;
+            }
+        }
+        else if(sprite.text.fill == currentPlayer){
         selectedTower = sprite;
+        }
       }
 }
 
@@ -40,11 +70,11 @@ function create() {
     var tower = towerGroup.children[i];
     if(i < towerGroup.children.length / 2){
     tower.text = game.add.text(tower.x + 50, 330, "",
-                        { font: "20px Arial", fill: "#ff0000", align: "center" });
+                        { font: "20px Arial", fill: player1, align: "center" });
     }
     else {
       tower.text = game.add.text(tower.x + 50, 330, "",
-                          { font: "20px Arial", fill: "#ffffFF", align: "center" });
+                          { font: "20px Arial", fill: player2, align: "center" });
     }
     tower.text.anchor.setTo(0.5, 0.5);
     tower.text.setText(tower.checkers);
